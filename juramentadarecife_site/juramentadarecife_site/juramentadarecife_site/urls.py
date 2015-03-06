ec7ba700f512b49f03301497797b32dcdeefe221
Anderson Berg <andersonberg@gmail.com>
2015-03-05 23:33:12 -0300

    página saiba mais

--- juramentadarecife_site/juramentadarecife_site/juramentadarecife_site/urls.py
+++ juramentadarecife_site/juramentadarecife_site/juramentadarecife_site/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^saiba-mais/', TemplateView.as_view(template_name='saiba_mais.html')),
 )
 
 # Uncomment the next line to serve media files in dev.
