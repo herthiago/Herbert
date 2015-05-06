016d670d9c4f2a910a24a613d9909fb853d70a94
Anderson Berg <andersonberg@gmail.com>
2015-05-06 09:59:40 -0300

    criando um fabfile

--- juramentadarecife_site/fabfile.py
+++ juramentadarecife_site/fabfile.py
@@ -1,13 +1,15 @@
 __author__ = 'anderson'
 
-from fabric.api import run
+from fabric.api import run, sudo, cd, env
 
 env.user = 'root'
 env.hosts = ['104.236.205.124']
 
 
 def git_pull():
-    run('sudo su - django')
-    run('cd juramentadarecife/')
-    result = run('git pull origin master')
-    print(result)
+    sudo('su - django')
+    with cd('juramentadarecife/'):
+        result = run('git pull origin master')
+        print(result)
+    disconnect_all()
+
